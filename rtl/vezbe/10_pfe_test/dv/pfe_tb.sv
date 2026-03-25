cf60e6dc6f4cca3a51f5ba977e0616cdccb36f23
nesto <ncudakzm@stb.com>
2026-03-25 22:33:54 +0100

    Kod za sortiranje - ima bagova samo moramo nesto da pushujemo

--- rtl/vezbe/10_pfe_test/dv/pfe_tb.sv
+++ rtl/vezbe/10_pfe_test/dv/pfe_tb.sv
@@ -3,8 +3,9 @@
 module pfe_tb;
 
     localparam int DSIZE = 8;
+    localparam int N = 8;
 
-    logic             clk_i      = 0;
+    logic             clk_i= 0;
     logic             rst_ni     = 0;
     logic [DSIZE-1:0] in_data_i  = '0;
     logic             in_valid_i = 0;
@@ -13,24 +14,45 @@ module pfe_tb;
     logic             out_valid_o;
     logic             out_ready_i = 1;
 
-    pfe #(.DSIZE(DSIZE)) dut (.*);
+    logic [DSIZE-1:0] sent[N];
+    logic [DSIZE-1:0] receved[N];
 
+    pfe #(.DSIZE(DSIZE), .N(N)) dut (.*);
+
+    // assign clk_i = 0;
     always #5 clk_i = ~clk_i;
 
     initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0,pfe_tb);
+
         repeat (2) @(posedge clk_i);
         rst_ni = 1;
 
-        for (int i = 0; i < 8; i++) begin
+        for (int i = 0; i < N; i++) begin
             @(negedge clk_i);
-            in_data_i  = $urandom_range(0, 255);
+            sent[i] = DSIZE'($urandom_range(0, 255));
+            in_data_i  = sent[i];
             in_valid_i = 1;
             @(posedge clk_i); #1;
-            assert (out_data_o === in_data_i) else $error("data mismatch: in=0x%02h out=0x%02h", in_data_i, out_data_o);
-            $display("data=0x%02h out=0x%02h %s",
-                     in_data_i, out_data_o, (out_data_o === in_data_i) ? "PASS" : "FAIL");
+            // assert (out_data_o === in_data_i) else $error("data mismatch: in=0x%02h out=0x%02h", in_data_i, out_data_o);
+            // $display("data=0x%02h out=0x%02h %s",
+            //          in_data_i, out_data_o, (out_data_o === in_data_i) ? "PASS" : "FAIL");
         end
 
+        @(posedge out_valid_o);
+        for (int i = 0; i < N; i++) begin
+            @(negedge clk_i);
+            receved[i] = out_data_o;
+            @(posedge clk_i); #1;
+        end
+
+        for (int i = 0; i < N; i++) begin
+            $display("data=0x%02h out=0x%02h",
+                     sent[i], receved[i]);
+        end
+
+        #10000;
         $display("Done"); $finish;
     end
 
--- rtl/vezbe/10_pfe_test/rtl/pfe.sv
+++ rtl/vezbe/10_pfe_test/rtl/pfe.sv
@@ -1,5 +1,6 @@
-module pfe #(
-    parameter int DSIZE  = 8
+ module pfe #(
+    parameter int DSIZE  = 8,
+    parameter int N = 8
 )(
     input  logic             clk_i,
     input  logic             rst_ni,
@@ -12,9 +13,99 @@ module pfe #(
     output logic             out_valid_o,
     input  logic             out_ready_i
 );
+    logic [DSIZE-1:0] reading_i[N];
+    logic [DSIZE-1:0] buff_o[N];
+    logic [DSIZE-1:0] info_i [N];
+    logic state;
 
-    assign in_ready_o = out_ready_i;
-    assign out_valid_o = in_valid_i;
-    assign out_data_o = in_data_i;
+    genvar i;
+    generate
+        for (i = 0; i < N / 2; i++) begin : g_for_cas
+            cas # (.N(DSIZE)) i_cas (
+                .A_i(info_i[i]),
+                .B_i(info_i[i+N/2]),
+                .S_i(1'b0),
+                .S_A_o(buff_o[i]),
+                .S_B_o(buff_o[i+N/2])
+            );
+        end
+    endgenerate
+
+    genvar j;
+    generate
+        for (j = 0; j < N; j++) begin : g_for_mux
+            mux # (.DSIZE(DSIZE)) i_mux (
+                .input_option1(reading_i[j]),
+                .input_option2(buff_o[j]),
+                .status(state),
+                .output_value(info_i[j])
+            );
+        end
+    endgenerate
+
+
+    typedef enum logic [1:0] {
+        READ    = 2'b00,
+        CALC    = 2'b01,
+        WRITE   = 2'b10,
+        DONE    = 2'b11
+    } state_t;
+    state_t current_state, next_state;
+    logic state_flag;
+
+    localparam int MEM = $clog2(N);
+    logic [MEM-1:0] ptr;
+
+    // komponenta za promenu stanja
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            current_state <= READ;
+        else if (state_flag)
+            current_state <= next_state;
+    end
+
+    // komponenta za proveru prelaska u drugo stanje
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            ptr <= 0;
+        else if (state_flag)
+            ptr <= 0;
+        else if (in_valid_i)
+            ptr <= ptr + 1;
+    end
+
+    always_comb begin
+        case (current_state)
+            READ: begin
+                state = 0;
+                out_valid_o = 0; //dodato
+                reading_i[ptr] = in_data_i;
+                next_state = CALC;
+                state_flag = (ptr === MEM'(N-1));
+            end
+
+            CALC: begin
+                state = 0;
+                next_state = WRITE;
+                state_flag = (ptr === MEM'(MEM));
+            end
+
+            WRITE: begin
+                out_data_o = info_i[ptr];
+                out_valid_o = 1;
+                next_state = DONE;
+                state_flag = (ptr === MEM'(N-1));
+            end
+
+            DONE: begin
+                // ne znam sta moze da radi kad zavrsi
+            end
+
+            default: begin
+                next_state = READ;
+            end
+        endcase
+    end
 
 endmodule
+
